32aec617c5c0f586cfd4abcddafd0d706ba4a816
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-11 11:36:45 +0800

    fix/ change integer to number

--- server/models/Author.js
+++ server/models/Author.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
   name: String,
-  age: Integer,
+  age: Number,
 });
 
 const Book = mongoose.model("Book", bookSchema);
