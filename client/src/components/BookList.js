3d293d0a7ef69fdf659564ab2cb34aafdf7aed06
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-14 20:43:28 +0800

    #26 - hook up front end and back end

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_BOOKS = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 export const BookList = () => {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  console.log(data);
+
   return (
     <div>
       <ul id="book-list">
--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-// require("dotenv").config();
+const cors = require("cors");
 
 const schema = require("./schemas/schema");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
