7d7f8df89fc5d2158f292d67a34f0e98f0cc6628
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-19 06:22:19 +0800

    fix variable injection

--- client/src/queries.js
+++ client/src/queries.js
@@ -20,7 +20,7 @@ export const GET_BOOKS = gql`
 
 export const MUTATION_ADD_BOOK = gql`
   mutation($name: String!, $genre: String!, $authorId: ID!) {
-    addBook(name: "", genre: "", authorId: "") {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
     }
